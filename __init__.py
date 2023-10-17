bc97353687aac45fda7f69458a72714a448fd74c
juanpnaranjo2006 <143004835+juanpnaranjo2006@users.noreply.github.com>
2023-10-17 08:47:57 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ def game():
     score = 0
     while True:
         print('======== Menu ========'
-        '\n1. Add'
+        '\n1. Modulus'
         '\n0. Exit')
         option = int(input('\nChoice an option: '))
         if option == 0:
