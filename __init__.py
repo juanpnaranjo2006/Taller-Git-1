3aada14a5457d5fa4253fed0c0d423635c2619e3
juanpnaranjo2006 <juanpnaranjo2006@javerianacali.edu.co>
2023-10-17 08:43:55 -0500

    Versión con potenciación

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from operations import division
+from operations import exponentiation
 def game():
     score = 0
     while True:
@@ -12,9 +12,9 @@ def game():
         num_2 = input('Enter second number: ')
         answer = int(input('Enter you answer: '))
         if option == 1:
-            result = division(num_1, num_2)
+            result = exponentiation(num_1, num_2)
             if result == answer:
-                score += 2
+                score += 4
                 print('Correct!!')
             else:
                 print('Incorrect')
