07d0c340a2664d118acca1118fb1a135bde48ad8
juanpnaranjo2006 <juanpnaranjo2006@javerianacali.edu.co>
2023-10-17 08:33:24 -0500

    Version del juego con resta

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from operations import add
+from operations import subtract
 def game():
     score = 0
     while True:
@@ -12,7 +12,7 @@ def game():
         num_2 = input('Enter second number: ')
         answer = int(input('Enter you answer: '))
         if option == 1:
-            result = add(num_1, num_2)
+            result = subtract(num_1, num_2)
             if result == answer:
                 score += 1
                 print('Correct!!')
