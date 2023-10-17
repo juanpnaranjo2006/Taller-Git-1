ccc305004b0cc77a7ee6cce54e77fc7d2af1ee2a
juanpnaranjo2006 <juanpnaranjo2006@javerianacali.edu.co>
2023-10-17 08:57:26 -0500

    Versión Final del juego

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
-from operations import mod
+from operations import add, subtract, multiply, division, exponentiation, mod
 def game():
     score = 0
     while True:
         print('======== Menu ========'
         '\n1. Add'
+        '\n2. Subtract'
+        '\n3. Multiply'
+        '\n4. Divide'
+        '\n5. Exponentiation'
+        '\n6. Modulus'
         '\n0. Exit')
         option = int(input('\nChoice an option: '))
         if option == 0:
@@ -12,6 +17,41 @@ def game():
         num_2 = input('Enter second number: ')
         answer = int(input('Enter you answer: '))
         if option == 1:
+            result = add(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+        elif option == 2:
+            result = subtract(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+        elif option == 3:
+            result = multiply(num_1, num_2)
+            if result == answer:
+                score += 2
+                print('Correct!!')
+            else:
+                print('Incorrect')
+        elif option == 4:
+            result = division(num_1, num_2)
+            if result == answer:
+                score += 2
+                print('Correct!!')
+            else:
+                print('Incorrect')
+        elif option == 5:
+            result = exponentiation(num_1, num_2)
+            if result == answer:
+                score += 4
+                print('Correct!!')
+            else:
+                print('Incorrect')
+        elif option == 6:
             result = mod(num_1, num_2)
             if result == answer:
                 score += 4
